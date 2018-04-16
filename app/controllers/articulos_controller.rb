8d564a2f0093363a8157f78d1fcbc8eac5ae4c18
Miguelmoreno13 <miguel.moreno.lt@gmail.com>
2018-04-16 03:57:53 +0000

    Agregar editar, mostrar e index

--- app/controllers/articulos_controller.rb
+++ app/controllers/articulos_controller.rb
@@ -1,6 +1,14 @@
 class ArticulosController < ApplicationController
+  def index
+    @articulos = Articulo.all
+  end
+  
   def new
     @articulo = Articulo.new
+  end
+  
+  def edit
+    @articulo = Articulo.find(params[:id])
   end
   
   def create
@@ -10,6 +18,16 @@ class ArticulosController < ApplicationController
        redirect_to articulo_path(@articulo)
     else
       render 'new'
+    end
+  end
+  
+  def update
+    @articulo = Articulo.find(params[:id])
+    if @articulo.update(articulo_parametros)
+      flash[:notice] = "El articulo fue actualizado correctamente"
+      redirect_to articulo_path(@articulo)
+    else
+      render 'edit'
     end
   end
   
