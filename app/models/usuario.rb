e10882ed02957da32ebcaeb194c8eed8108f5ffd
Miguelmoreno13 <miguel.moreno.lt@gmail.com>
2018-04-29 18:33:19 +0000

    Agregando contraseña segura

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -9,4 +9,5 @@ class Usuario < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { maximum: 105 },
             format: { with: VALID_EMAIL_REGEX }
+  has_secure_password
 end
