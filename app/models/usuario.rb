4e7bd64f8f2e364a4857f13b9bf718b3313c640a
Miguelmoreno13 <miguel.moreno.lt@gmail.com>
2018-04-29 05:33:04 +0000

    Validacion de usuarios

--- app/models/usuario.rb
+++ app/models/usuario.rb
@@ -1,3 +1,10 @@
 class Usuario < ApplicationRecord
-
+  validates :nusuario, presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: {minimum: 5, maximum: 50}
+  VALID_EMAIL_REGEX = /\A[\w\-.]+@[a-z\d\-.]+\.[a-z]+\z/i    
+  validates :email, presence: true, 
+            uniqueness: { case_sensitive: false },
+            length: { maximum: 105 },
+            format: { with: VALID_EMAIL_REGEX }
 end
