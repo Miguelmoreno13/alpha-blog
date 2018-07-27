b49bc435f613b7575d0edee4a892f7d82f610023
Miguelmoreno13 <miguel.moreno.lt@gmail.com>
2018-07-27 03:49:05 +0000

    Agregando requerimientos de admin para crear categorias

--- app/controllers/categorias_controller.rb
+++ app/controllers/categorias_controller.rb
@@ -1,4 +1,6 @@
 class CategoriasController < ApplicationController
+  before_action :require_admin, except: [:index, :show]
+  
   def index
     @categorias = Categoria.paginate(page: params[:page], per_page: 5)
   end
@@ -23,6 +25,13 @@ class CategoriasController < ApplicationController
   private
   def categoria_params
     params.require(:categoria).permit(:nombre)
+  end
+  
+  def require_admin
+    if !logged_in? || (logged_in? and !current_user.admin?)
+      flash[:danger] = "Solo el administrador puede realizar esa accion"
+      redirect_to categorias_path
+    end
   end
   
 end
--- test/controllers/categorias_controller_test.rb
+++ test/controllers/categorias_controller_test.rb
@@ -4,6 +4,7 @@ class CategoriasControllerTest < ActionDispatch::IntegrationTest
   
   def setup
     @categoria = Categoria.create(nombre: "deportes")
+    @user = Usuario.create(usuario: "migue", email: "migue@ejemplo.com", password: "contra", admin: true)
   end
   
   test "debe tener indice de categorias" do
@@ -12,6 +13,7 @@ class CategoriasControllerTest < ActionDispatch::IntegrationTest
   end
   
   test "debe tener nuevo" do
+    sing_in_as(@user, "contra")
     get new_categoria_path
     assert_response :success
   end
@@ -19,6 +21,13 @@ class CategoriasControllerTest < ActionDispatch::IntegrationTest
   test "debe mostrar" do
     get categoria_path(@categoria)
     assert_response :success
+  end
+  
+  test "debe de redireccionar al crear cuando el admin no esta conectado" do 
+    assert_no_difference 'Categoria.count' do
+      post categorias_path, params: { categoria: {nombre: "deportes"} }
+    end
+    assert_redirected_to categorias_path
   end
   
 end
