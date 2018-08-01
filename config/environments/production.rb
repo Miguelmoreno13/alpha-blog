4477b4ecf68a922957a611577e4fd40471b3c89f
Miguelmoreno13 <miguel.moreno.lt@gmail.com>
2018-08-01 15:39:26 +0000

    Probando mailer #4

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,15 +31,7 @@ Rails.application.configure do
   config.assets.compile = false
   
   config.action_mailer.default_url_options = {:host =>'https://alpha-blog-miguel.herokuapp.com'}
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-  :address              => "smtp.gmail.com",
-  :port                 => 587,
-  :domain               => "alpha-blog-miguel.herokuapp.com",
-  :user_name            => "carl.was.here.before@gmail.com",
-  :password             => "Dont3verBother$",
-  :authentication       => 'plain',
-  :enable_starttls_auto => true  }
+  config.action_mailer.delivery_method = :sendmail
   
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
