2547ed82d22a36355b3da785085e104e6198c7ab
Miguelmoreno13 <miguel.moreno.lt@gmail.com>
2018-09-02 23:49:30 +0000

    Prueba exitosa espero Zoho

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,7 +27,7 @@ Rails.application.configure do
   end
   
   # Configure default URL for action mailer
-  config.action_mailer.default_url_options = {:host =>'https://eabcf9debea04750b136f5b0ac7a50b2.vfs.cloud9.us-east-2.amazonaws.com'}
+  config.action_mailer.default_url_options = {:host =>'https://alpha-blog-miguel.herokuapp.com'}
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
   address:              'smtp.zoho.com',
