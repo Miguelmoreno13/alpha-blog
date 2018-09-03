c9d7af73f01f7a77abfcec1ef32f4b49c13f1780
Miguelmoreno13 <miguel.moreno.lt@gmail.com>
2018-09-03 00:15:31 +0000

    Prueba Mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -39,8 +39,6 @@ Rails.application.configure do
   user_name:            'ayuda_soporte@zoho.com',
   password:             'Amores2011',
   authentication:       'plain',
-  ssl: true,
-  tls: true,
   enable_starttls_auto: true }
   
   config.action_mailer.perform_deliveries = true
