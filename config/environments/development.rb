bfa4c87a979765d8f2d743de8b8dc9bb69c857ce
Miguelmoreno13 <miguel.moreno.lt@gmail.com>
2018-12-23 23:03:41 +0000

    Contrasenia olvidadas

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -28,17 +28,17 @@ Rails.application.configure do
   
   # Configure default URL for action mailer
   config.action_mailer.default_url_options = {:host =>'https://eabcf9debea04750b136f5b0ac7a50b2.vfs.cloud9.us-east-2.amazonaws.com'}
-config.action_mailer.raise_delivery_errors = true
-config.action_mailer.delivery_method = :smtp
-config.action_mailer.smtp_settings = {
-  address:              'smtp.gmail.com',
-  port:                 587,
-  domain:               'example.com',
-  user_name:            ENV['EMAIL_USER'],
-  password:             ENV['EMAIL_PASS'],
-  authentication:       'plain',
-  enable_starttls_auto: true
-}
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'example.com',
+    user_name:            ENV['EMAIL_USER'],
+    password:             ENV['EMAIL_PASS'],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 
 
   # Print deprecation notices to the Rails logger.
